8abde75884c9c1cf34e4db3b32d6c11f748192c3
Sam <thesamreaves@gmail.com>
2015-11-19 01:53:31 -0500

    Added proper testing. LOVING this SuperTest library

--- src/server/test/test.js
+++ src/server/test/test.js
@@ -35,24 +35,84 @@ describe('server tests', function () {
 	});
     
 	// Make sure server responds to root
-    it('responds to /', function testRoot(done) {
-  	    request(server)
-    		.get('/')
-    		.expect(200, done);
-  	});
+  it('responds to /', function testRoot(done) {
+	    request(server)
+  		.get('/')
+      .expect('Content-Type', /html/)
+  		.expect(200, done);
+	});
 
-    // Make sure server responds to products endpoint
-  	it('responds to /api/products', function testProducts(done) {
-  	    request(server)
-    		.get('/api/products')
-    		.expect(200, done);
-  	});
+  // Make sure server responds to products endpoint and with correct response
+	it('responds to /api/products', function testProducts(done) {
+	    
+      // Mock required API response
+      var products = [
+        {
+          id: 1,
+          name: "BRnr Cord",
+          dimensions: "4'x 4'x 8'",
+          price: 500, 
+          volume: "128 cubic feet"
+        },
+        {
+          id: 2,
+          name: "BRnr Half Cord",
+          dimensions: "4'x 4'x 4'",
+          price: 350, 
+          volume: "64 cubic feet"
+        },
+        {
+          id: 3,
+          name: "BRnr Face Cord",
+          dimensions: "4'x 8'x 16\"",
+          price: 310, 
+          volume: "42.6 Cubic Feet"
+        },
+        {
+          id: 4,
+          name: "BRnr Quarter Cord",
+          dimensions: "4'x 6'x 16\"",
+          price: 275, 
+          volume: "64 cubic feet"
+        },
+        {
+          id: 5,
+          name: "BRnr White Birch Firewood",
+          dimensions: "4'x 4'x 16\"",
+          price: 275, 
+          volume: "42.6 Cubic Feet"
+        }
+      ];
 
-  	// Make sure server responds to products by id endpoint
+      // Fire off request
+      request(server)
+  		.get('/api/products')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect(products, done);
+	});
+
+
+  	// Make sure server responds to products by id endpoint and with correct response
   	it('responds to /api/products/:product_id', function testProductsByID(done) {
-  	    request(server)
-    		.get('/api/products')
-    		.expect(200, done);
+  	    
+        // Mock required response 
+        var product = {
+          id: 1,
+          name: "BRnr Cord",
+          dimensions: "4'x 4'x 8'",
+          price: 500, 
+          volume: "128 cubic feet"
+        };
+
+        // Fire off request
+        request(server)
+    		.get('/api/products/1')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(product, done)
   	});
 
   	// Make sure server does not responds to bullshit endpoint
