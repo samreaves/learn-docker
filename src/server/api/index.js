838beae1073080ae7f7bd7d5c1ca2ec6fce7ccac
Sam <thesamreaves@gmail.com>
2015-11-19 01:48:19 -0500

    Added comments. Removed unnecessary dependencies

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -16,13 +16,15 @@ var products = require('./controllers/products.js');
 
 // Initialize express, file system, lodash, API route and users cache
 var express = require('express'),
-	fs = require('fs'),
-	path = require('path'),
     api_route = express.Router();
 
+// Send 404 on root/api
 api_route.get("/", function(req, res) {
 	res.send(404);
 })
+
+// Use products endpoint at root/api/products
 api_route.use('/products', products);
 
+// Export this route
 module.exports = api_route;
